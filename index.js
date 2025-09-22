4a3d88a6ed9c1143d1347bea5f6829f9f3c844f0
Ryan Gilbert <ryangilbert7926@gmail.com>
2025-09-22 13:40:43 -0400

    add cdp

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+import structuredClone from '@ungap/structured-clone';
+import { install } from 'react-native-quick-crypto';
+import 'react-native-get-random-values';
+import 'text-encoding';
+import 'react-native-url-polyfill/auto';
+
+// eslint-disable-next-line no-undef
+if (!('structuredClone' in globalThis)) {
+  // eslint-disable-next-line no-undef
+  globalThis.structuredClone = structuredClone;
+}
+
+install();
 /**
  * @format
  */
